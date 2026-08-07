6d5a0a81333e2d40eb7758986f52453de24a6b0b
Krxypton <krxypton@gmail.com>
2026-08-07 23:10:53 +0100

    Added opening times to the location page

--- src/pages/location.js
+++ src/pages/location.js
@@ -14,13 +14,32 @@ export default function buildLocation() {
     strip.classList.add('strip')
     background.appendChild(strip)
 
+    // tells location
+    const leftLocation =  document.createElement('div')
+    leftLocation.classList.add('left-location')
+    strip.appendChild(leftLocation)
     const titleLocation = document.createElement('h1') 
     titleLocation.classList.add('title-location')
     titleLocation.textContent =  "Location"
-    strip.appendChild(titleLocation)
+    leftLocation.appendChild(titleLocation)
 
     const address = document.createElement('div') //We write the address here
     address.classList.add('address')
-    strip.appendChild(address)
+    leftLocation.appendChild(address)
     address.innerHTML = '📍 25 Caldera View Oia,<br> Santorini 847 02,<br> Greece '
+
+    // Opening Times
+
+    const rightLocation = document.createElement('div')
+    rightLocation.classList.add('right-location')
+    strip.appendChild(rightLocation)
+    const titleOP= document.createElement('h1')
+    titleOP.classList.add('title-times')
+    titleOP.textContent = 'Opening Times'
+    rightLocation.appendChild(titleOP)
+
+    const times = document.createElement('div')
+    times.classList.add('opening-times')
+    rightLocation.appendChild(times)
+    times.innerHTML = 'Monday – Thursday : 12:00 PM – 10:30 PM <br>Friday – Saturday : 12:00 PM – 11:30 PM <br>Sunday : 12:00 PM – 10:00 PM'
 }
